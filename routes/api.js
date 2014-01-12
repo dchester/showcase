1855ae943cb5fbfc0993cab71545aca0fb52fdf5
David Chester <dchester@shutterstock.com>
2014-01-11 19:18:28 -0500

    add API write routes

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,106 @@ exports.initialize = function(app) {
 	var models = app.dreamer.models;
 	var workspaceLoader = app.showcase.middleware.workspaceLoader;
 
-	app.get('/api/:workspace_handle/:collection_handle', workspaceLoader, function* (req, res) {
+	app.post('/api/:workspace_handle/:collection_handle', workspaceLoader, function*(req, res) {
+
+		var workspace = req.showcase.workspace;
+		var status = req.body._status;
+		var user_id = req.body._user_id;
+		var collection_name = req.params.collection_handle;
+		var data = req.body;
+
+		if (!user_id) {
+			return res.json(400, {
+				message: "_user_id is required",
+				code: "user_id_required"
+			});
+		}
+
+		var collection = yield Collection.load({ name: collection_name });
+
+		var item = yield Item.build({
+			collection_id: collection.id,
+			status: status,
+			data: data,
+			user_id: user_id
+		});
+
+		if (!item) return res.json(404, {
+			message: "couldn't find item",
+			code: "no_item_found"
+		});
+
+		var errors = item.validate();
+
+		if (errors) return res.json(400, {
+			message: "validation failed",
+			code: "validation_failed",
+			errors: errors
+		});
+
+		yield item.save({ user_id: user_id });
+
+		res.json(201, Item.distill(item));
+	});
+
+	app.delete('/api/:workspace_handle/:collection_handle/:item_id', workspaceLoader, function*(req, res) {
+
+		var workspace = req.showcase.workspace;
+		var item_id = req.params.item_id;
+
+		var item = yield Item.load({ id: item_id });
+
+		if (!item) return res.json(404, {
+			message: "couldn't find item",
+			code: "no_item_found"
+		});
+
+		yield item.destroy();
+		res.send(204);
+	});
+
+	app.post('/api/:workspace_handle/:collection_handle/:item_id', workspaceLoader, function*(req, res) {
+
+		var workspace = req.showcase.workspace;
+		var item_id = req.params.item_id;
+		var status = req.body._status;
+		var user_id = req.body._user_id;
+		var data = req.body;
+
+		if (!user_id) {
+			return res.json(400, {
+				message: "_user_id is required",
+				code: "user_id_required"
+			});
+		}
+
+		var item = yield Item.load({ id: item_id });
+
+		if (!item) return res.json(404, {
+			message: "couldn't find item",
+			code: "no_item_found"
+		});
+
+		item.update({
+			status: status,
+			data: req.body,
+			user_id: user_id,
+		});
+
+		var errors = item.validate();
+
+		if (errors) return res.json(400, {
+			message: "validation failed",
+			code: "validation_failed",
+			errors: errors
+		});
+
+		yield item.save({ user_id: user_id });
+
+		res.json(Item.distill(item));
+	});
+
+	app.get('/api/:workspace_handle/:collection_handle', workspaceLoader, function*(req, res) {
 
 		var name = req.params.collection_handle;
 		var per_page = req.query.per_page || 40;
