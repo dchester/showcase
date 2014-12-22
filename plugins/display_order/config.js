a11b3d83f36d3c24d7c8365e2d7911b7914aba5e
David Chester <david@fmail.co.uk>
2014-12-21 21:29:50 -0500

    tweak display order logic

--- plugins/display_order/config.js
+++ plugins/display_order/config.js
@@ -7,11 +7,16 @@ module.exports = {
 		callback(Number(item.data[field.name]) + Number(item.id));
 	},
 	deflate: function(field, item, models, callback) {
-		callback(Number(item.data[field.name]) - Number(item.id));
+		var value = item.data[field.name];
+		if (value == 'initial' || value == null) {
+			callback(0);
+		} else {
+			callback(Number(value) - Number(item.id));
+		}
 	},
 	preview: function(value) {
 		value = value || 0;
-		return '<span class="display-order" data-initial-value="' + value + '"><span class="tooltip">Sort by this column to reorder</span></span>';
+		return '<span class="display-order" data-initial-value="' + value + '">' + value + '<span class="tooltip">Sort by this column to reorder</span></span>';
 	}
 };
 
